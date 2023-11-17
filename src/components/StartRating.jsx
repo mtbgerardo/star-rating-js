5429ceb40dd761e930e74ca2c089c9247a554662
mtbgerardo <mtbgerardo@gmail.com>
2023-11-17 13:01:35 -0600

    Add hover effect

--- src/components/StartRating.jsx
+++ src/components/StartRating.jsx
@@ -2,20 +2,23 @@ import { useState } from "react";
 
 const StartRating = () => {
   const [rating, setRaiting] = useState(0);
+  const [hover, setHover] = useState(0);
   return (
     <div className="startRating">
       {[...Array(5)].map((star, index) => {
         index += 1;
         return (
-        <button
+          <button
             type="button"
             key={index}
-            className={index <= rating ? "on" : "off"}
+            className={index <= (hover || rating) ? "on" : "off"}
             onClick={() => setRaiting(index)}
+            onMouseEnter={() => setHover(index)}
+            onMouseLeave={() => setHover(rating)}
           >
             <span className="start">&#9733;</span>
           </button>
-          )
+        );
       })}
     </div>
   );
